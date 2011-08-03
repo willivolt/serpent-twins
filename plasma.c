739439cc7a7faccae7f4d816625d446c5e1a0fcb
Ka-Ping Yee <kpy@google.com>
2011-08-03 12:24:46 -0700

    Add plasma.  Get rid of sunset-palette and replace it with sunset-palette-2.

---
+++ plasma.c
@@ -0,0 +1,33 @@
+#include "serpent.h"
+#include <math.h>
+#include "spectrum-palette.h"
+#include "sunset-palette.h" 
+#define PALETTE SPECTRUM_PALETTE
+
+float sin_table[256];
+byte pixels[NUM_PIXELS*3];
+
+#define SIN(n) sin_table[((int) (n)) & 0xff]
+
+void next_frame(int f) {
+  if (f == 0) {
+    for (int i = 0; i < 256; i++) {
+      sin_table[i] = sin(i/256.0*2*M_PI);
+    }
+  }
+
+  for (int r = 0; r < NUM_ROWS; r++) {
+    for (int c = 0; c < NUM_COLUMNS; c++) {
+      float altitude =
+          SIN((r*0.7 - c + f*2)*2.9) +
+          SIN((SIN(r*4)*40 + c*1.2 + f)*4.7) +
+          SIN((c - r*0.4 - f*0.7)*2.1) *
+          SIN((SIN(c*3.4 + r*0.3)*20 - f*2)*5.1);
+      set_from_palette(pixels, pixel_index(r, c), PALETTE, 0.5 + altitude*0.3);
+    }
+  }
+
+  for (int s = 0; s < NUM_SEGS; s++) {
+    put_pixels(s, pixels + s*SEG_PIXELS*3, SEG_PIXELS);
+  }
+}
--- serpent.h
+++ serpent.h
@@ -2,13 +2,33 @@
 #define M_PI 3.1415926535897932384626433832795
 #endif
 
+#define NUM_SEGS 10 // number of segments in the serpent
+#define SEG_ROWS 12 // rows (rings) of pixels in one segment
+#define NUM_ROWS (NUM_SEGS*SEG_ROWS) // rows of pixels in the entire serpent
+#define NUM_COLUMNS 25 // number of pixels in one ring
+#define SEG_PIXELS (SEG_ROWS*NUM_COLUMNS) // number of pixels in one segment
+#define NUM_PIXELS (NUM_SEGS*SEG_PIXELS) // number of pixels in entire serpent
+
 typedef unsigned char byte;
 
+#define pixel_index(row, col) \
+   ((row)*NUM_COLUMNS + (((row) % 2) ? (NUM_COLUMNS - 1 - (col)) : (col)))
+
 #define set_rgb(pixels, index, r, g, b) { \
-  byte* p = pixels + (index)*3; \
-  *(p++) = r; \
-  *(p++) = g; \
-  *(p++) = b; \
+  byte* __p = (pixels) + (index)*3; \
+  *(__p++) = r; \
+  *(__p++) = g; \
+  *(__p++) = b; \
+}
+
+#define palette_index(palette, f) ((int) (((f) - floor(f))*palette##_SIZE))
+
+#define set_from_palette(pixels, pixel_index, palette, f) { \
+  byte* __s = (palette) + palette_index(palette, f)*3; \
+  byte* __d = (pixels) + (pixel_index)*3; \
+  *(__d++) = *(__s++); \
+  *(__d++) = *(__s++); \
+  *(__d++) = *(__s++); \
 }
 
 void put_pixels(int segment, byte* pixels, int n);
