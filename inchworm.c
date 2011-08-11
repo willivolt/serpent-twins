663b8d26db5a8bd0f2a10c42a26098331df436ab
Ka-Ping Yee <kpy@google.com>
2011-08-11 10:37:36 -0700

    Add inchworm.

---
+++ inchworm.c
@@ -0,0 +1,28 @@
+#include "serpent.h"
+#include "spectrum.pal"
+
+#define REV 2*M_PI
+
+byte pixels[NUM_ROWS*NUM_COLUMNS*3];
+
+void next_frame(int f) {
+    float t = -f*REV/100;
+    for (int r = 0; r < NUM_ROWS; r++) {
+        float u = r/5.0;
+        float v = r/20.0;
+        float w = sin(u*REV + sin(v + t)*16);
+        float p = (pow(2, pow(2, w)) - 1.0)/3.0;
+        p = (0.5 + p*0.4);
+        int red = 127;
+        int green = p*255;
+        int blue = 127;
+        if (p < 0.3) { red = green = 0; }
+        for (int c = 0; c < NUM_COLUMNS; c++) {
+            set_rgb(pixels, pixel_index(r, c), red, green, blue);
+            set_from_palette(pixels, pixel_index(r, c), SPECTRUM_PALETTE, p);
+        }
+    }
+    for (int s = 0; s < 10; s++) {
+        put_pixels(s, pixels + s*SEG_PIXELS*3, SEG_PIXELS);
+    }
+}
