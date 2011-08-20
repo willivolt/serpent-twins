395d75f672f5fa88ae6e49cd5b716ef96517ce1c
Ka-Ping Yee <kpy@google.com>
2011-08-19 20:05:58 -0700

    Add multichannel output for Chumby.

--- total_control.h
+++ total_control.h
@@ -14,3 +14,9 @@ void tcl_put_pixel(byte red, byte green, byte blue);
 
 // Send an entire sequence of n pixels, given n*3 bytes of colour data.
 void tcl_put_pixels(byte* pixels, int n);
+
+// Send multiple pixels in parallel, one pixel to each strand.
+void tcl_put_pixel_multi(byte** pixel_ptrs, int num_strands);
+
+// Send multiple sequences of pixels in parallel to multiple strands.
+void tcl_put_pixels_multi(byte** pixel_ptrs, int num_strands, int num_pixels);
