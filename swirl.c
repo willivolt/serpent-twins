68d6b71ef88fd3dff69b889f3e65b7dbafed8ef5
Ka-Ping Yee <kpy@google.com>
2011-08-24 22:26:42 -0700

    Add interactive control to swirl.

--- swirl.c
+++ swirl.c
@@ -24,25 +24,36 @@
 #define SINE_WAVE_PERIOD 40
 #define DUTY_CYCLE_ON 120
 #define DUTY_CYCLE_OFF 120
+#define IMPULSE_START 100
+#define BUTTON_FORCE 10
 
 #define TICKS_PER_FRAME 10 
 #define FPS 20
 #define SPRING_CONSTANT 400  // kg/s^2
 #define MASS 0.1  // kg
-#define FRICTION_FORCE 0.02  // kg*rev^2/s
+#define FRICTION_FORCE 0.03  // kg*rev^2/s
 #define FRICTION_MIN_VELOCITY 0.01  // rev/s
 
 unsigned char pixels[NUM_PIXELS*3];
 float position[NUM_ROWS];  // rev
 float velocity[NUM_ROWS];  // rev/s
+int auto_impulse = 1;
 
 void set_hue(int row, int col, float hue) {
   set_from_palette(pixels, pixel_index(row, col), PALETTE, hue - floor(hue));
 }
 
 void tick(float dt) {
-  for (int i = 1; i < NUM_ROWS; i++) {
-    float force = SPRING_CONSTANT * (position[i-1] - position[i]);
+  for (int i = 0; i < NUM_ROWS; i++) {
+    float force;
+    if (i == 0) {
+      if (auto_impulse) {
+        continue;
+      }
+      force = (read_button('b') - read_button('a'))*BUTTON_FORCE;
+    } else {
+      force = SPRING_CONSTANT * (position[i-1] - position[i]);
+    }
     if (i < NUM_ROWS - 1) {
       force += SPRING_CONSTANT * (position[i+1] - position[i]);
     }
@@ -53,23 +64,31 @@ void tick(float dt) {
     }
     velocity[i] += force/MASS * dt;
   }
-  for (int i = 1; i < NUM_ROWS; i++) {
+  for (int i = 0; i < NUM_ROWS; i++) {
     position[i] += velocity[i] * dt;
   }
 }
 
 void next_frame(int x) {
   if (x == 0) {
+    auto_impulse = 1;
     for (int i = 0; i < NUM_ROWS; i++) {
       position[i] = 0;
       velocity[i] = 0;
     }
   }
 
-  float duty_phase = x % (DUTY_CYCLE_ON + DUTY_CYCLE_OFF);
-  if (duty_phase < DUTY_CYCLE_ON) {
-    position[0] = sin(2*M_PI*(duty_phase/SINE_WAVE_PERIOD))*SINE_WAVE_AMPLITUDE;
+  if (auto_impulse) {
+    float duty_phase = (x - IMPULSE_START) % (DUTY_CYCLE_ON + DUTY_CYCLE_OFF);
+    if (x > IMPULSE_START && duty_phase < DUTY_CYCLE_ON) {
+      position[0] = sin(
+          2*M_PI*(duty_phase/SINE_WAVE_PERIOD))*SINE_WAVE_AMPLITUDE;
+    }
   }
+  if (read_button('a') || read_button('b')) {
+    auto_impulse = 0;
+  }
+  
   for (int t = 0; t < TICKS_PER_FRAME; t++) {
     tick(1.0/FPS/TICKS_PER_FRAME);
   }
