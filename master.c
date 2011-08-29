bf3de81e920ebf48bd0bdcde6668a3264014b462
Ka-Ping Yee <kpy@google.com>
2011-08-29 02:46:36 -0700

    Integrate swirl into master.

--- master.c
+++ master.c
@@ -15,12 +15,13 @@
 // limitations under the License.
 
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "serpent.h"
 
-//#define SEC FPS  // use this for animation time parameters
-#define SEC 6  // use this to speed up by a factor of 10 for testing
+#define SEC FPS  // use this for animation time parameters
+//#define SEC 6  // use this to speed up by a factor of 10 for testing
 
 
 // Pixel manipulation ======================================================
@@ -56,6 +57,12 @@ long __value;
   pixels[__i].b = __value < 0 ? 0 : __value > 255 ? 255 : __value; \
 }
 
+// sets the colour of a pixel from a palette
+#define paint_from_palette(pixels, pixel_index, palette, f, alpha) { \
+  byte* __s = (palette) + palette_index(palette, f)*3; \
+  paint_rgb(pixels, pixel_index, __s[0], __s[1], __s[2], alpha); \
+}
+
 
 // Common initialization ===================================================
 
@@ -82,16 +89,18 @@ void init_tables() {
 struct pattern;
 typedef byte next_frame_func(struct pattern* p, pixel* pixels);
 struct pattern {
+  char* name;
   next_frame_func* next_frame;
   long frame;
 };
+typedef struct pattern pattern;
 
-short transition_alpha(long frame, long stop_frame, long transition_period) {
-  if (frame > stop_frame + transition_period) {
+short transition_alpha(long frame, long duration, long transition_period) {
+  if (frame > transition_period + duration + transition_period) {
     return -1;
   }
-  if (frame > stop_frame) {
-    return 256 - EASE(frame - stop_frame, transition_period);
+  if (frame > transition_period + duration) {
+    return 256 - EASE(frame - transition_period - duration, transition_period);
   }
   if (frame < transition_period) {
     return EASE(frame, transition_period);
@@ -106,7 +115,7 @@ short transition_alpha(long frame, long stop_frame, long transition_period) {
 
 // Null pattern ============================================================
 
-byte null_next_frame(struct pattern* p, pixel* pixels) {
+byte null_next_frame(pattern* p, pixel* pixels) {
   return 0;
 }
 
@@ -126,7 +135,7 @@ void base_select_target(pixel* target_pixels) {
   }
 }
 
-byte base_next_frame(struct pattern* p, pixel* pixels) {
+byte base_next_frame(pattern* p, pixel* pixels) {
   static pixel source_pixels[NUM_PIXELS];
   static pixel target_pixels[NUM_PIXELS];
   static long stop_frame = 0;
@@ -153,9 +162,126 @@ byte base_next_frame(struct pattern* p, pixel* pixels) {
 }
 
 
+// Swirl pattern ===========================================================
+
+#include "spectrum.pal"
+#include "sunset.pal"
+#define SWIRL_PALETTE SPECTRUM_PALETTE
+
+#define SWIRL_DUTY_CYCLE_ON 120
+#define SWIRL_DUTY_CYCLE_OFF 120
+#define SWIRL_IMPULSE_START (60*SEC)
+
+#define SWIRL_TICKS_PER_FRAME 10 
+#define SWIRL_SPRING_CONST 400  // kg/s^2
+#define SWIRL_MASS 0.1  // kg
+
+float swirl_position[NUM_ROWS];  // rev
+float swirl_velocity[NUM_ROWS];  // rev/s
+int swirl_auto_impulse = 1;
+float swirl_button_force = 30;
+float swirl_restore_factor = 1;
+float swirl_restore_center = 0;
+float swirl_auto_impulse_period = 40;
+float swirl_auto_impulse_amplitude = 0.9;
+
+void swirl_tick(float dt) {
+  float friction_force = 0.05 + read_button('y')*0.2;
+  float friction_min_velocity = friction_force/SWIRL_MASS * dt;
+
+  for (int i = 0; i < NUM_ROWS; i++) {
+    float force;
+    if (i == 0) {
+      force = accel_right()*0.7 +
+          (read_button('b') - read_button('a'))*swirl_button_force;
+      if (force < -25 || force > 25) {
+        swirl_auto_impulse = 0;
+      }
+      if (swirl_auto_impulse) {
+        continue;
+      }
+    } else {
+      force = SWIRL_SPRING_CONST * (swirl_position[i-1] - swirl_position[i]);
+    }
+    if (i < NUM_ROWS - 1) {
+      force += SWIRL_SPRING_CONST * (swirl_position[i+1] - swirl_position[i]);
+    }
+    force += swirl_restore_factor * (swirl_restore_center - swirl_position[i]);
+    if (swirl_velocity[i] > friction_min_velocity) {
+      force -= friction_force;
+    } else if (swirl_velocity[i] < -friction_min_velocity) {
+      force += friction_force;
+    }
+    swirl_velocity[i] += force/SWIRL_MASS * dt;
+  }
+  for (int i = 0; i < NUM_ROWS; i++) {
+    swirl_position[i] += swirl_velocity[i] * dt;
+  }
+}
+
+byte swirl_next_frame(pattern* p, pixel* pixels) {
+  int x = p->frame;
+  short alpha = get_alpha_or_terminate(x, 60*SEC, 10*SEC);
+
+  static int left_outer_eye_start = 182;
+  static int right_outer_eye_start = 182 + 22 + 13 + 12 + 6;
+  static int outer_eye_length = 22;
+  static int inner_eye_length = 13;
+
+  if (x == 0) {
+    swirl_auto_impulse = 1;
+    for (int i = 0; i < NUM_ROWS; i++) {
+      swirl_position[i] = 0;
+      swirl_velocity[i] = 0;
+    }
+  }
+
+  if (swirl_auto_impulse) {
+    float duty_phase = (x - SWIRL_IMPULSE_START) %
+        (SWIRL_DUTY_CYCLE_ON + SWIRL_DUTY_CYCLE_OFF);
+    if (x > SWIRL_IMPULSE_START && duty_phase < SWIRL_DUTY_CYCLE_ON) {
+      swirl_position[0] = swirl_auto_impulse_amplitude *
+          sin(2*M_PI*(duty_phase/swirl_auto_impulse_period));
+    } else {
+      swirl_auto_impulse_period = (random() % 30) + 30;
+      swirl_auto_impulse_amplitude = (random() % 10)*0.1 + 0.5;
+    }
+  }
+  if (read_button('a') || read_button('b')) {
+    swirl_auto_impulse = 0;
+  }
+  if (read_button('x')) {
+    swirl_button_force = 10;
+    swirl_restore_factor = 0;
+  }
+  if (read_button('y')) {
+    swirl_button_force = 30;
+    swirl_restore_factor = 1;
+    double sum = 0;
+    for (int i = 0; i < NUM_ROWS; i++) {
+      sum += swirl_position[i];
+    }
+    swirl_restore_center = sum / NUM_ROWS;
+  }
+  
+  for (int t = 0; t < SWIRL_TICKS_PER_FRAME; t++) {
+    swirl_tick(1.0/FPS/SWIRL_TICKS_PER_FRAME);
+  }
+
+  for (int i = 0; i < NUM_ROWS; i++) {
+    for (int j = 0; j < NUM_COLUMNS; j++) {
+      paint_from_palette(pixels, pixel_index(i, j), SWIRL_PALETTE,
+                         swirl_position[i] + (float) j / NUM_COLUMNS + 0.5,
+                         alpha);
+    }
+  }
+
+  return 1;
+}
+
 // Flash pattern ===========================================================
 
-byte flash_next_frame(struct pattern* p, pixel* pixels) {
+byte flash_next_frame(pattern* p, pixel* pixels) {
   add_to_pixel(pixels, random() % NUM_PIXELS, 255, 255, 255, 256);
 
   return p->frame < 60*SEC;
@@ -164,28 +290,31 @@ byte flash_next_frame(struct pattern* p, pixel* pixels) {
 
 // Pattern table ===========================================================
 
-struct pattern NULL_PATTERN = {null_next_frame, 0};
-
-struct pattern BASE_PATTERN = {base_next_frame, 0};
+pattern BASE_PATTERN = {"base", base_next_frame, 0};
 
 #define NUM_PATTERNS 1
-struct pattern PATTERNS[] = {
+pattern PATTERNS[] = {
 //  {plasma_next_frame, 0},
-  {flash_next_frame, 0}
+  {"swirl", swirl_next_frame, 0}
 };
 
 
 // Master routine ==========================================================
 
-static struct pattern* curp = &NULL_PATTERN;
+static pattern* curp = NULL;
 
-void activate_pattern(struct pattern* p) {
+void activate_pattern(pattern* p) {
+  printf("\nactivating %s\n", p->name);
   curp = p;
   curp->frame = 0;
 }
 
 void next_frame(int frame) {
-  static long time_to_next_pattern = 60*SEC;
+  static long time_to_next_pattern = 5*SEC;
+  static int left_outer_eye_start = 182;
+  static int right_outer_eye_start = 182 + 22 + 13 + 12 + 6;
+  static int outer_eye_length = 22;
+  static int inner_eye_length = 13;
 
   if (frame == 0) {
     init_tables();
@@ -194,19 +323,52 @@ void next_frame(int frame) {
   base_next_frame(&BASE_PATTERN, pixels);
   BASE_PATTERN.frame++;
 
-  if ((curp->next_frame)(curp, pixels)) {
-    curp->frame++;
+  if (curp) {
+    if ((curp->next_frame)(curp, pixels)) {
+      curp->frame++;
+    } else {
+      printf("\nfinished %s\n", curp->name);
+      curp = NULL;
+    }
   } else {
-    curp = &NULL_PATTERN;
     if (time_to_next_pattern) {
       time_to_next_pattern--;
     } else {
       activate_pattern(PATTERNS + random() % NUM_PATTERNS);
-      time_to_next_pattern = 300*SEC;
+      time_to_next_pattern = 60*SEC;
     }
   }
 
   for (int s = 0; s < NUM_SEGS; s++) {
     put_segment_pixels(s, (byte*) (pixels + s*SEG_PIXELS), SEG_PIXELS);
   }
+
+  if (strcmp(get_button_sequence(), "xxaxa") == 0) {
+    left_outer_eye_start -= 1;
+    clear_button_sequence();
+  }
+  if (strcmp(get_button_sequence(), "xxaxb") == 0) {
+    left_outer_eye_start += 1;
+    clear_button_sequence();
+  }
+  if (strcmp(get_button_sequence(), "xxbxa") == 0) {
+    right_outer_eye_start -= 1;
+    clear_button_sequence();
+  }
+  if (strcmp(get_button_sequence(), "xxbxb") == 0) {
+    right_outer_eye_start += 1;
+    clear_button_sequence();
+  }
+
+  // overwrite the eyes
+  pixel* p = (pixel*) pixels;
+  for (int i = 0; i < outer_eye_length; i++) {
+    p[left_outer_eye_start + i] = p[0];
+    p[right_outer_eye_start + i] = p[0];
+  }
+  for (int i = 0; i < inner_eye_length; i++) {
+    p[left_outer_eye_start + outer_eye_length + i] = p[NUM_PIXELS - 1];
+    p[right_outer_eye_start + outer_eye_length + i] = p[NUM_PIXELS - 1];
+  }
+  put_head_pixels((byte*) pixels, HEAD_PIXELS);
 }
