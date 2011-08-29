023516ee65a70a9ed27c63287e3e4ae4f3a23fa6
Ka-Ping Yee <kpy@google.com>
2011-08-28 18:59:45 -0700

    Rename stuff in swirl to make a swirl namespace.

--- swirl.c
+++ swirl.c
@@ -16,114 +16,114 @@
 
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 #include "serpent.h"
 #include "spectrum.pal"
 #include "sunset.pal"
-#define PALETTE SPECTRUM_PALETTE
+#define SWIRL_PALETTE SPECTRUM_PALETTE
 
-#define DUTY_CYCLE_ON 120
-#define DUTY_CYCLE_OFF 120
-#define IMPULSE_START 100
+#define SWIRL_DUTY_CYCLE_ON 120
+#define SWIRL_DUTY_CYCLE_OFF 120
+#define SWIRL_IMPULSE_START 100
 
-#define TICKS_PER_FRAME 10 
-#define FPS 20
-#define SPRING_CONSTANT 400  // kg/s^2
-#define MASS 0.1  // kg
-#define FRICTION_MIN_VELOCITY 0.02  // rev/s
+#define SWIRL_TICKS_PER_FRAME 10 
+#define SWIRL_SPRING_CONST 400  // kg/s^2
+#define SWIRL_MASS 0.1  // kg
 
 unsigned char pixels[NUM_PIXELS*3];
-float position[NUM_ROWS];  // rev
-float velocity[NUM_ROWS];  // rev/s
-int auto_impulse = 1;
-float button_force = 30;
-float restore_factor = 1;
-float restore_center = 0;
-float auto_impulse_period = 40;
-float auto_impulse_amplitude = 0.9;
+float swirl_position[NUM_ROWS];  // rev
+float swirl_velocity[NUM_ROWS];  // rev/s
+int swirl_auto_impulse = 1;
+float swirl_button_force = 30;
+float swirl_restore_factor = 1;
+float swirl_restore_center = 0;
+float swirl_auto_impulse_period = 40;
+float swirl_auto_impulse_amplitude = 0.9;
 
-void tick(float dt) {
+void swirl_tick(float dt) {
   float friction_force = 0.05 + read_button('y')*0.2;
-  float friction_min_velocity = friction_force/MASS * dt;
+  float friction_min_velocity = friction_force/SWIRL_MASS * dt;
 
   for (int i = 0; i < NUM_ROWS; i++) {
     float force;
     if (i == 0) {
       force = accel_right()*0.7 +
-          (read_button('b') - read_button('a'))*button_force;
+          (read_button('b') - read_button('a'))*swirl_button_force;
       if (force < -25 || force > 25) {
-        auto_impulse = 0;
+        swirl_auto_impulse = 0;
       }
-      if (auto_impulse) {
+      if (swirl_auto_impulse) {
         continue;
       }
     } else {
-      force = SPRING_CONSTANT * (position[i-1] - position[i]);
+      force = SWIRL_SPRING_CONST * (swirl_position[i-1] - swirl_position[i]);
     }
     if (i < NUM_ROWS - 1) {
-      force += SPRING_CONSTANT * (position[i+1] - position[i]);
+      force += SWIRL_SPRING_CONST * (swirl_position[i+1] - swirl_position[i]);
     }
-    force += restore_factor * (restore_center - position[i]);
-    if (velocity[i] > friction_min_velocity) {
+    force += swirl_restore_factor * (swirl_restore_center - swirl_position[i]);
+    if (swirl_velocity[i] > friction_min_velocity) {
       force -= friction_force;
-    } else if (velocity[i] < -friction_min_velocity) {
+    } else if (swirl_velocity[i] < -friction_min_velocity) {
       force += friction_force;
     }
-    velocity[i] += force/MASS * dt;
+    swirl_velocity[i] += force/SWIRL_MASS * dt;
   }
   for (int i = 0; i < NUM_ROWS; i++) {
-    position[i] += velocity[i] * dt;
+    swirl_position[i] += swirl_velocity[i] * dt;
   }
 }
 
-static int left_outer_eye_start = 182;
-static int right_outer_eye_start = 182 + 22 + 13 + 12 + 6;
-static int outer_eye_length = 22;
-static int inner_eye_length = 13;
-
 void next_frame(int x) {
+  static int left_outer_eye_start = 182;
+  static int right_outer_eye_start = 182 + 22 + 13 + 12 + 6;
+  static int outer_eye_length = 22;
+  static int inner_eye_length = 13;
+
   if (x == 0) {
-    auto_impulse = 1;
+    swirl_auto_impulse = 1;
     for (int i = 0; i < NUM_ROWS; i++) {
-      position[i] = 0;
-      velocity[i] = 0;
+      swirl_position[i] = 0;
+      swirl_velocity[i] = 0;
     }
   }
 
-  if (auto_impulse) {
-    float duty_phase = (x - IMPULSE_START) % (DUTY_CYCLE_ON + DUTY_CYCLE_OFF);
-    if (x > IMPULSE_START && duty_phase < DUTY_CYCLE_ON) {
-      position[0] = sin(
-          2*M_PI*(duty_phase/auto_impulse_period))*auto_impulse_amplitude;
+  if (swirl_auto_impulse) {
+    float duty_phase = (x - SWIRL_IMPULSE_START) %
+        (SWIRL_DUTY_CYCLE_ON + SWIRL_DUTY_CYCLE_OFF);
+    if (x > SWIRL_IMPULSE_START && duty_phase < SWIRL_DUTY_CYCLE_ON) {
+      swirl_position[0] = swirl_auto_impulse_amplitude *
+          sin(2*M_PI*(duty_phase/swirl_auto_impulse_period));
     } else {
-      auto_impulse_period = (random() % 30) + 30;
-      auto_impulse_amplitude = (random() % 10)*0.1 + 0.5;
+      swirl_auto_impulse_period = (random() % 30) + 30;
+      swirl_auto_impulse_amplitude = (random() % 10)*0.1 + 0.5;
     }
   }
   if (read_button('a') || read_button('b')) {
-    auto_impulse = 0;
+    swirl_auto_impulse = 0;
   }
   if (read_button('x')) {
-    button_force = 10;
-    restore_factor = 0;
+    swirl_button_force = 10;
+    swirl_restore_factor = 0;
   }
   if (read_button('y')) {
-    button_force = 30;
-    restore_factor = 1;
+    swirl_button_force = 30;
+    swirl_restore_factor = 1;
     double sum = 0;
     for (int i = 0; i < NUM_ROWS; i++) {
-      sum += position[i];
+      sum += swirl_position[i];
     }
-    restore_center = sum / NUM_ROWS;
+    swirl_restore_center = sum / NUM_ROWS;
   }
   
-  for (int t = 0; t < TICKS_PER_FRAME; t++) {
-    tick(1.0/FPS/TICKS_PER_FRAME);
+  for (int t = 0; t < SWIRL_TICKS_PER_FRAME; t++) {
+    swirl_tick(1.0/FPS/SWIRL_TICKS_PER_FRAME);
   }
 
   for (int i = 0; i < NUM_ROWS; i++) {
     for (int j = 0; j < NUM_COLUMNS; j++) {
-      set_from_palette(pixels, pixel_index(i, j), PALETTE,
-                       position[i] + (float) j / NUM_COLUMNS + 0.5);
+      set_from_palette(pixels, pixel_index(i, j), SWIRL_PALETTE,
+                       swirl_position[i] + (float) j / NUM_COLUMNS + 0.5);
     }
   }
 
