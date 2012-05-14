183db88bef90448e0a265df55304bf2fc81b31dd
Ka-Ping Yee <kpy@google.com>
2012-05-13 23:39:17 -0700

    Whenever the socket connection goes down, repeatedly try to reconnect. Use a blocking send to force all the data to get sent. Ignore SIGPIPE so we don't crash when the socket is closed.

--- tcp_pixels.c
+++ tcp_pixels.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
 
 #ifndef TYPEDEF_BYTE
 #define TYPEDEF_BYTE
@@ -11,8 +14,9 @@ typedef unsigned char byte;
 
 int sock = -1;
 
-int tcp_init(char* ipaddr, int port) {
-  sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+int tcp_connect(char* ipaddr, int port) {
+  char buffer[100];
+  int sock = -1;
   struct sockaddr_in address;
   address.sin_family = AF_INET;
   address.sin_port = htons(port);
@@ -20,12 +24,54 @@ int tcp_init(char* ipaddr, int port) {
     fprintf(stderr, "Invalid IP address: %s\n", ipaddr);
     return -1;
   }
+  sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (connect(sock, (struct sockaddr*) &address, sizeof(address)) != 0) {
-    fprintf(stderr, "Could not connect to %s port %d\n", ipaddr, port);
-    perror("Error");
+    close(sock);
+    sprintf(buffer, "Could not connect to %s port %d", ipaddr, port);
+    perror(buffer);
     return -1;
   }
+  fprintf(stderr, "Connected to %s port %d\n", ipaddr, port);
   return sock;
+}
+
+void tcp_init() {
+  FILE* fp;
+  int count;
+  char ipaddr[20];
+  int port;
+
+  /* Ignore the SIGPIPE that occurs when we write to a closed socket. */
+  signal(SIGPIPE, SIG_IGN);
+
+  while (sock < 0) {
+    usleep(50000); /* 0.05 sec */
+    fp = fopen("/tmp/serpent", "r");
+    if (!fp) {
+      perror("Cannot read /tmp/serpent");
+      continue;
+    }
+    count = fscanf(fp, "%16s %d", ipaddr, &port);
+    fclose(fp);
+    if (count != 2) {
+      fprintf(stderr, "Could not parse /tmp/serpent\n");
+      continue;
+    }
+    sock = tcp_connect(ipaddr, port);
+  }
+}
+
+void tcp_send_sync(const byte* buf, int len) {
+  int sent = 0;
+  int result;
+  while (sock >= 0 && sent < len) {
+    result = send(sock, buf + sent, len - sent, MSG_DONTWAIT);
+    if (result < 0) {
+      close(sock);
+      sock = -1;
+    }
+    sent += result;
+  }
 }
 
 void tcp_put_pixels(byte address, byte* pixels, int n) {
@@ -35,8 +81,8 @@ void tcp_put_pixels(byte address, byte* pixels, int n) {
   header[1] = 0;
   header[2] = length >> 8;
   header[3] = length & 0xff;
-  write(sock, header, 4);
-  write(sock, pixels, length);
+  tcp_send_sync(header, 4);
+  tcp_send_sync(pixels, length);
 }
 
 void tcp_put_pixels_multi(byte** pixel_ptrs, int num_channels, int num_pixels) {
