c04ab290bd64925e5498547d7bc874e2b5699ae4
Ka-Ping Yee <kpy@google.com>
2012-05-26 01:50:50 -0700

    Add text rendering!

---
+++ font.h
@@ -0,0 +1,17 @@
+#include "serpent.h"
+
+typedef struct {
+  byte width;
+  byte height;
+  byte min_char;
+  byte num_chars;
+  byte* data;
+} font;
+
+font* font_read(char* filename, int min_char, int char_width);
+
+void font_delete(font* f);
+
+typedef void put_pixel_func(int x, int y, byte r, byte g, byte b);
+
+void font_draw(font* f, char* str, int x, int y, put_pixel_func* put_pixel, byte r, byte g, byte b);
---
+++ text.c
@@ -0,0 +1,43 @@
+// "text", by Ka-Ping Yee
+
+// Copyright 2011 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "serpent.h"
+#include "font.h"
+
+unsigned char pixels[NUM_PIXELS*3];
+
+void put_pixel(int x, int y, byte r, byte g, byte b) {
+  set_rgb_rc(pixels, x, y, r, g, b);
+}
+
+font* f = NULL;
+
+void next_frame(int x) {
+  if (x == 0) {
+    f = font_read("lucidatypewriter.pgm", 32, 6);
+    fprintf(stderr, "%p\n", f);
+  }
+
+  font_draw(f, "Boiling Miles", x % 34, 13, put_pixel, 255, 255, 255);
+
+  for (int s = 0; s < 10; s++) {
+    put_segment_pixels(s, pixels + s*SEG_PIXELS*3, SEG_PIXELS);
+  }
+}
