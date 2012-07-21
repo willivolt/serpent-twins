25171c4d0cb97a6d433d8d6dc32a09ade8882fd6
Ka-Ping Yee <kpy@google.com>
2012-07-21 13:32:31 -0700

    Add barrel address initialization sequence.

--- tcp_pixels.h
+++ tcp_pixels.h
@@ -1,3 +1,4 @@
 void tcp_init();
+void tcp_init_addresses();
 void tcp_put_pixels(byte address, byte* pixels, int n);
 void tcp_put_pixels_multi(byte** pixel_ptrs, int num_channels, int num_pixels);
